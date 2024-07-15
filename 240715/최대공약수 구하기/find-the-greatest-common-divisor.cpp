9fb9250a72e3217ed5ef8f5818d6059ed34406bc
sn25yb <162320560+sn25yb@users.noreply.github.com>
2024-07-15 12:06:07 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    int res;
+
+    cin >> n >> m;
+    for (res = min(n, m); res > 0; res--)
+        if (!(m % res) && !(n % res))
+            break;
+    cout << res;
+    return (0);
+}
